f898401b4e5bd631d00aafa4ef880938753dfe81
Geoffrey Demaison <geoffrey.demaiso@gmail.com>
2021-07-26 07:07:57 +0200

    debug prints removed

--- src/scene_parser/get_variables.c
+++ src/scene_parser/get_variables.c
@@ -7,7 +7,6 @@ int		find_var_line(const char *var_name, unsigned int *index, char **lines)
 		if (ft_strlen(lines[i]) > (ft_strlen("set ") + ft_strlen(var_name))
 			&& ft_strncmp(lines[i], "set ", 4) == 0)
 		{
-			printf("%s | %s\n", &lines[i][4], var_name);
 			if (ft_strncmp(&lines[i][4], var_name, ft_strlen(var_name)) == 0)
 			{
 				*index = i;
@@ -44,7 +43,6 @@ int		get_variables(t_rt_env *env, char **lines)
 
 	for (unsigned int i = 0; i < NB_SCENE_VARS; i++)
 	{
-		printf("There\n");
 		if ((ret = find_var_line(var_names[i], &line_index, lines)))
 			return (ret);
 		if ((ret = var_parsing_fts[i](env, lines[line_index])))
